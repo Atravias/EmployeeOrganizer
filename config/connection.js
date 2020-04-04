409b779f767ddb391cd7d379fba85f89a07a62ee
Atravias Borley <atravias@gmail.com>
2020-04-04 17:25:54 -0600

    first commit. got first prompts working

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Borleatr000",
+    password: process.env.MYSQL_PASSWORD,
     database: "employeeTrackerdb"
 })
-console.log(process.env.MYSQL_PASSWORD)
+
 
 connection.connect(function (err) {
     if (err) throw err;
